dda754c0b133766bf602157406af78ab1958c3bd
James Sigler <jamussigler@gmail.com>
2020-05-01 00:03:29 -0400

    Converted Video player to generic file viewer
    File viewer is it's own component that loads in the different viewer
    types into it's body through slots. This way we have one parent
    component for the dialog

--- VueServer/ClientApp/components/pages/browser.vue
+++ VueServer/ClientApp/components/pages/browser.vue
@@ -4,14 +4,30 @@
                        parentView="browser"
                        @loadFile="loadFile"></file-explorer>
 
-        <video-player :url="path" :on="on" @player-off="on = false"></video-player>
+        <file-viewer :open="dialogOpen" @viewer-off="dialogOpen = false">
+            <template v-if="type === mediaTypes.video">
+                <p slot="header">Video Player</p>
+                <video-player :url="path" :on="on" @player-off="on = false"></video-player>
+            </template>
+            <template v-else-if="type === mediaTypes.image">
+                <p slot="header">Image Viewer</p>
+                <img :src="path" />
+            </template>
+            <template v-else-if="type === mediaTypes.text">
+                <p slot="header">Text Viewer</p>
+                <p>{{ path }}</p>
+            </template>
+        </file-viewer>
     </div>
 </template>
 
 <script>
     import Service from '../../services/file-explorer'
+
     import Explorer from '../modules/file-explorer'
-    import VideoPlayer from '../modules/video-player'
+    import Viewer from '../modules/viewers/file-viewer'
+    import VideoPlayer from '../modules/viewers/video-player'
+
     import { Roles } from '../../constants'
 
     export default {
@@ -20,12 +36,21 @@
                 level: 0,
                 folders: [],
 
-                path: '',
-                on: false
+                type: null,
+                path: null,
+                on: false,
+                dialogOpen: false,
+
+                mediaTypes: {
+                    video: 'video',
+                    image: 'image',
+                    text: 'text'
+                }
             }
         },
         components: {
             'file-explorer': Explorer,
+            'file-viewer': Viewer,
             'video-player': VideoPlayer,
         },
         created() {
@@ -46,6 +71,26 @@
 
             this.getFolders();
         },
+        watch: {
+            dialogOpen(newValue) {
+                // Close dialog
+                if (newValue === false) {
+                    // If our player isn't on
+                    if (!this.on) {
+                        const self = this;
+                        // Turn it on so when we turn it off it'll flip the watcher
+                        this.on = true;
+                        setTimeout(() => {
+                            self.on = false;
+                        }, 50);
+                    }
+                    // Otherwise just turn it off
+                    else {
+                        this.on = false;
+                    }
+                }
+            }
+        },
         methods: {
             async getFolders() {
                 await Service.getFolderList().then(resp => {
@@ -54,11 +99,54 @@
                     this.$_console_log("Failed to upload file");
                 });
             },
+            getType(file) {
+                if (typeof file === 'undefined' || file === '') {
+                    this.$_console_log('[File Viewer] getType: Undefined or empty');
+                    return this.mediaTypes.text;
+                }
+
+                if (!file.includes('.')) {
+                    this.$_console_log('[File Viewer] getType: Doesn\'t include a period (.) ');
+                    return this.mediaTypes.text;
+                }
+
+                const index = file.lastIndexOf('.');
+                const extension = file.slice(index).toLowerCase();
+
+                // TODO: Create exhaustive list of extensions
+                switch (extension) {
+                    case '.mkv':
+                    case '.avi':
+                    case '.mpeg':
+                    case '.mpg':
+                    case '.mp4':
+                    case '.wmv':
+                    case '.webm':
+                    case '.mp3':
+                        return this.mediaTypes.video;
+                    case '.jpg':
+                    case '.jpeg':
+                    case '.gif':
+                    case '.tiff':
+                    case '.bmp':
+                    case '.png':
+                    case '.img':
+                        return this.mediaTypes.image;
+                    default:
+                        return this.mediaTypes.text;
+                }
+            },
             loadFile(file) {
-                // Add check to load different modules depending on what the file type is
                 this.$_console_log('[BROWSER] LoadFile: ', file);
+
+                this.type = this.getType(file);
+
                 this.path = file;
-                this.on = true;
+                this.dialogOpen = true;
+                setTimeout(() => {
+                    this.on = true;
+                }, 15);
+                
             }
         }
     }
